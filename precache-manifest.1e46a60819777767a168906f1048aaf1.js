025135d64cd81026bf6a1867fdbd40968206e27b
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-06-18 13:35:40 -0400

    Updates

---
+++ precache-manifest.1e46a60819777767a168906f1048aaf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bcb4a42322bf4355af00407dac0d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e4a6a7c2da9fbaf1e8",
+    "url": "/static/css/main.11de08c7.chunk.css"
+  },
+  {
+    "revision": "d237ffc08b0cdfab8dc9",
+    "url": "/static/js/2.bf483065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf483065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e4a6a7c2da9fbaf1e8",
+    "url": "/static/js/main.775508d7.chunk.js"
+  },
+  {
+    "revision": "2a79bdc0414322758a09",
+    "url": "/static/js/runtime-main.0dfb9a00.js"
+  }
+]);
