36a0dada03adf3efd04acadaa4d8aa9242ef0812
Mike Verdicchio <MikeVerdicchio@users.noreply.github.com>
2020-06-19 19:42:08 -0400

    Filter results to user's district (#3)
    * Remove secrets
    * WIP filtering results
    * Update constants
    * Add content_security_policy
    * Refactor and cleanup
    * Reorg lists
    * Remove async

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,9 @@
 export const CANDIDATE_DATA_URL =
   "https://civic-coup.github.io/data/candidates/index.json";
 
-// Keys :(
-export const GOOGLE_PLACES_API_KEY = "AIzaSyBd_1eJQIXma_-FkL88k8DrVU0b6RfvOXc";
-export const GOOGLE_API_KEY = "AIzaSyCgRnwb1bj2OXkG0ELSPUNq9IZj141FoWk";
+// Google API Information
+export const GOOGLE_API_SCRIPT_URL = "https://apis.google.com/js/api.js";
+export const GOOGLE_CIVIC_INFO_URL =
+  "https://content.googleapis.com/discovery/v1/apis/civicinfo/v2/rest";
+export const GOOGLE_PLACES_API_KEY = "SECRET";
+export const GOOGLE_API_KEY = "SECRET";
