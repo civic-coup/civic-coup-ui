d17662bec83a48c8cde97b9fd15de6164551fc79
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-06-19 14:32:28 -0400

    Remove secrets

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ export const CANDIDATE_DATA_URL =
   "https://civic-coup.github.io/data/candidates/index.json";
 
 // Keys :(
-export const GOOGLE_PLACES_API_KEY = "AIzaSyBd_1eJQIXma_-FkL88k8DrVU0b6RfvOXc";
-export const GOOGLE_API_KEY = "AIzaSyCgRnwb1bj2OXkG0ELSPUNq9IZj141FoWk";
+export const GOOGLE_PLACES_API_KEY = "SECRET";
+export const GOOGLE_API_KEY = "SECRET";
