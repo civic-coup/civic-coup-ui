7d67e53e5cda6230d5a2e47fc9cb6cfac28bdb8f
Denise Montesdeoca <denise@Denises-MacBook-Air.local>
2020-06-19 14:41:30 -0400

    linting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import AddressForm from "../AddressForm";
 import CandidateListing from "../CandidateListing";
 
-const pictureOne = require("../../img/about.jpg");
+const pictureOne = require("../../img/cause.jpeg");
 
 function App() {
   const [address, setAddress] = useState("");
@@ -12,24 +12,15 @@ function App() {
   return (
     <div className="main">
       <section className="bg-cover bg-center hero">
-        <div className="dark-overlay" />
         <div className="position-relative z-index-1">
-          <div className="container text-center text-white">
-            <p className="font-italic lead">Oh, hello, nice to meet you!</p>
-            <h1 className="text-uppercase my-4">My name is screen</h1>
+          <div className="container text-center text-black">
+            <h1 className="text-uppercase my-4">Civic Coup</h1>
             <p className="font-italic lead">
-              I am a nice scrolling template prepared for portfolio, telling
-              stories and short presentations.
-            </p>
-            <p className="font-italic lead">
-              I have best results with nice pictures on the background or nice
-              colours.
+              Knowledge is power, change starts with you.
             </p>
           </div>
         </div>
-        <div className="scroll-btn link-scroll">
-          <i className="fas fa-angle-double-down" />
-        </div>
+        {/* <div className="scroll-btn link-scroll"><i className="fas fa-angle-double-down"></i></div> */}
       </section>
       <section id="1">
         <div className="d-flex h-100 align-items-center">
@@ -37,10 +28,10 @@ function App() {
             <div className="row align-items-center">
               <div className="col-lg-6 mb-4 mb-lg-0">
                 <header className="text-center">
-                  <h2 className="text-uppercase lined">About Me</h2>
+                  <h2 className="text-uppercase lined">Cause</h2>
                 </header>
                 <p className="lead">
-                  DENISE This can be an about section or anything else ;)
+                  This can be an about section or anything else ;)
                 </p>
                 <p>
                   Impossible considered invitation him men instrument saw
@@ -73,20 +64,90 @@ function App() {
       <section className="bg-gray">
         <div className="d-flex h-100 align-items-center">
           <div className="container">
-            <header className="mb-5 text-center">
-              <h2 className="text-uppercase lined">
-                Find you district candidates!
-              </h2>
-            </header>
-            <div className="App">
-              <header className="App-header">
-                {address ? (
-                  <CandidateListing address={address} />
-                ) : (
-                  <AddressForm onLocatePlace={(input) => setAddress(input)} />
-                )}
+            <header className="App-header">
+              <header className="text-center mb-5">
+                <h2 className="text-uppercase lined">
+                  Find your district candidates
+                </h2>
               </header>
+              {address ? (
+                <CandidateListing address={address} />
+              ) : (
+                <AddressForm onLocatePlace={(input) => setAddress(input)} />
+              )}
+            </header>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="d-flex h-100 align-items-center">
+          <div className="container">
+            <header className="text-center mb-5">
+              <h2 className="text-uppercase lined">Contact</h2>
+            </header>
+            <div className="row">
+              <p>
+                Effects present letters inquiry no an removed or friends. Desire
+                behind latter me though in. Supposing shameless am he engrossed
+                up additions. My possible peculiar together to. Desire so better
+                am cannot he up before points. Remember mistaken opinions it
+                pleasure of debating. Court front maids forty if aware their at.
+                Chicken use are pressed removed.
+              </p>
+              <p>
+                Able an hope of body. Any nay shyness article matters own
+                removal nothing his forming. Gay own additions education
+                satisfied the perpetual. If he cause manor happy. Without
+                farther she exposed saw man led. Along on happy could cease
+                green oh.
+              </p>
+              <ul className="mb-0 list-inline text-center">
+                <li className="list-inline-item">
+                  <a
+                    href="https://github.com/civic-coup"
+                    className="social-link social-link-twitter"
+                  >
+                    <i className="fab fa-twitter" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://github.com/civic-coup"
+                    className="social-link social-link-instagram"
+                  >
+                    <i className="fab fa-instagram" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://github.com/civic-coup"
+                    className="social-link social-link-email"
+                  >
+                    <i className="fas fa-envelope" />
+                  </a>
+                </li>
+              </ul>
             </div>
+            <footer className="py-5 mt-5">
+              <div className="row">
+                <div className="col-lg-6 text-center text-lg-left">
+                  <p className="font-italic mb-0 text-gray">
+                    &copy; 2020 Civic Coup
+                  </p>
+                </div>
+                <div className="col-lg-6 text-center text-lg-right">
+                  <p className="font-italic mb-0 text-gray">
+                    &copy; Template by
+                    <a
+                      href="https://bootstrapious.com/p/big-bootstrap-tutorial"
+                      className="text-gray"
+                    >
+                      Bootstrapious
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </footer>
           </div>
         </div>
       </section>
