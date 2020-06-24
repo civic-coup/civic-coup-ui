1e2b3aea6ab0a70224f3a6e996e7d8eb5ae841d7
Denise Montesdeoca <denise.j.montesdeoca@gmail.com>
2020-06-24 17:48:20 -0400

    main page (#4)
    * add resource files for bootstrap template
    
    * fix lint issue
    
    * linting
    
    * add link to image
    
    * add classnames
    
    * remove pictures not being used and rename main picture
    
    * capitalize the E
    
    Co-authored-by: Denise Montesdeoca <denise@Denises-MacBook-Air.local>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import classNames from "classnames";
 
 import "./App.css";
 import AddressForm from "../AddressForm";
@@ -8,14 +9,35 @@ function App() {
   const [address, setAddress] = useState("");
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {address ? (
-          <CandidateListing address={address} />
-        ) : (
-          <AddressForm onLocatePlace={(input) => setAddress(input)} />
-        )}
-      </header>
+    <div className="main">
+      <section className={classNames("bg-cover", "bg-center", "hero")}>
+        <div className={classNames("main-page", "container")}>
+          <div
+            className={classNames(
+              "text-center",
+              "text-white",
+              "text-uppercase",
+              "my-3",
+              "top"
+            )}
+          >
+            <h1>Civic Coup</h1>
+            <h8>Choose the right local government</h8>
+          </div>
+          <div className={classNames("text-center", "text-white")}>
+            <p className={classNames("font-italic", "lead", "m3")}>
+              Enter your address to see who is running in your district
+            </p>
+            <div className="App-header">
+              {address ? (
+                <CandidateListing address={address} />
+              ) : (
+                <AddressForm onLocatePlace={(input) => setAddress(input)} />
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
